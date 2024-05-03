64ebbfa7ae2d0465c1dde9538780b3626f0dd029
Brionna <brimorris317@gmail.com>
2024-05-03 06:18:13 -0400

    added the render code from the site

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+// your code
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
+
 <style>
   .navbar 
     overflow: hidden;
